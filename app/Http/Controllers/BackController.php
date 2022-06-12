f635d2d7db010a51e037cd25a911803ca63f1948
Fathur Walkers <fathurwalkers44@gmail.com>
2022-06-12 00:50:44 -0700

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use App\Models\Login;
+use App\Models\Data;
+use App\Models\Histori;
 
 class BackController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\BackController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => '/dashboard'], function () {
+    // DASHBOARD ROUTE
+    Route::get('/', [BackController::class, 'index'])->name('dashboard');
 });
