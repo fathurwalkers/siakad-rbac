01ca3275b47c34f12c4f2f3b936e7c3036095de4
Fathur Walkers <fathurwalkers44@gmail.com>
2022-10-07 13:38:06 -0700

    update

--- database/migrations/2022_10_07_203106_create_nilais_table.php
+++ database/migrations/2022_10_07_203106_create_nilais_table.php
@@ -8,14 +8,31 @@ class CreateNilaisTable extends Migration
 {
     public function up()
     {
-        Schema::create('nilais', function (Blueprint $table) {
+        Schema::create('nilai', function (Blueprint $table) {
             $table->id();
+
+            $table->string('nilai_siswa_tugas')->nullable();
+            $table->string('nilai_siswa_absensi')->nullable();
+            $table->string('nilai_siswa_uts')->nullable();
+            $table->string('nilai_siswa_uas')->nullable();
+            $table->string('nilai_ratarata')->nullable();
+            $table->time('nilai_waktu');
+            $table->date('nilai_tanggal');
+
+            $table->unsignedBigInteger('matapelajaran_id')->nullable();
+            $table->unsignedBigInteger('guru_id')->nullable();
+            $table->unsignedBigInteger('siswa_id')->nullable();
+
+            $table->foreign('matapelajaran_id')->references('id')->on('matapelajaran');
+            $table->foreign('guru_id')->references('id')->on('guru');
+            $table->foreign('siswa_id')->references('id')->on('siswa');
+
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('nilais');
+        Schema::dropIfExists('nilai');
     }
 }
