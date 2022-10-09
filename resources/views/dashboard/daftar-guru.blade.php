9d745d4d1dd33a4735279db286224952b2116297
Fathur Walkers <fathurwalkers44@gmail.com>
2022-10-09 13:29:53 -0700

    update

---
+++ resources/views/dashboard/daftar-guru.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.dashboard-layout')
+
+@section('title', 'Dashboard - Daftar Guru')
+
+@push('css')
+    <link href="{{ asset('datatables') }}/datatables.min.css" rel="stylesheet">
+@endpush
+
+@section('content-title', 'Dashboard - Daftar Guru')
+
+{{-- ------------------- main content ------------------- --}}
+@section('main-content')
+
+    <div class="row mt-1 mb-1">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="container">
+                        <div class="row">
+                            <h4>
+                                <b>
+                                    Daftar Guru
+                                </b>
+                            </h4>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <div class="table-responsive">
+                                <table id="example" class="display table-bordered" style="width:100%">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Nama</th>
+                                            <th>NIP</th>
+                                            <th>Jenis Kelamin</th>
+                                            <th>No. Telepon</th>
+                                            <th>Mata Pelajaran</th>
+                                            <th>Kelas</th>
+                                            <th>Kelola</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($siswa as $item)
+                                            <tr>
+                                                <td class="text-center">{{ $loop->iteration }}</td>
+                                                <td>{{ $item->guru_nama }}</td>
+                                                <td>{{ $item->guru_nip }}</td>
+                                                <td>{{ $item->guru_jeniskelamin }}</td>
+                                                <td>{{ $item->guru_telepon }}</td>
+                                                <td>{{ $item->matapelajaran->matapelajaran_nama }}</td>
+                                                <td>{{ $item->kelas->kelas_nama }}</td>
+                                                <td>
+                                                    <div class="row">
+                                                        <div
+                                                            class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center mx-auto">
+                                                            <button href="#"
+                                                                class="btn btn-sm btn-primary mr-1">Lihat</button>
+                                                            <button href="#" class="btn btn-sm btn-success mr-1">Ubah</button>
+                                                            <button href="#" class="btn btn-sm btn-danger">Hapus</button>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+{{-- ------------------- end main content ------------------- --}}
+
+@push('js')
+    <script src="{{ asset('datatables') }}/datatables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+@endpush
