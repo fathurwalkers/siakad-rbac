f55a084a302f773b9e094c3d9603ede2d95c9e29
Fathur Walkers <fathurwalkers44@gmail.com>
2022-10-28 03:42:04 -0700

    update

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class NilaiController extends Controller
 {
-    //
+    public function daftar_nilai()
+    {
+        $session_users = session('data_login');
+        $users = Login::find($session_users->id);
+        $nilai = Nilai::all();
+        return view('dashboard.daftar-nilai', [
+            'users' => $users,
+            'nilai' => $nilai,
+        ]);
+    }
 }
