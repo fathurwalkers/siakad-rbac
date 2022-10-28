2571679c5643fc72c2432e7f88c803ed7386a529
Fathur Walkers <fathurwalkers44@gmail.com>
2022-10-28 00:28:57 -0700

    update

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+use Illuminate\Support\Arr;
+use App\Models\Login;
+use App\Models\Absensi;
+use App\Models\Guru;
+use App\Models\Siswa;
+use App\Models\Kelas;
+use App\Models\Matapelajaran;
+use App\Models\Nilai;
+use App\Models\Semester;
+use Illuminate\Support\Facades\Redis;
 
 class KelasController extends Controller
 {
-    //
+    public function daftar_kelas()
+    {
+        $session_users = session('data_login');
+        $users = Login::find($session_users->id);
+        return view('dashboard.daftar-kelas', [
+            'users' => $users
+        ]);
+    }
 }
